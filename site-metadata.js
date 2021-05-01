0c6c04d06c3ed1c8d7b5ea6da804c5e3fd67c97b
Clarice Bouwer <cbillowes@gmail.com>
2021-05-01 06:51:18 +0400

    Create site-metadata.js

---
+++ site-metadata.js
@@ -0,0 +1,10 @@
+export default {
+  brand: `Curious Programmer`,
+  title: `A curious place for a curious mind.`,
+  description: `I share my thoughts. I share my ideas. I share my what I think is cool.`,
+  siteUrl: `https://curiousprogrammer.dev`,
+  author: `Clarice Bouwer`,
+  handle: `@cbillowes`,
+  image: `/share.jpg`,
+  lang: "en-US",
+}
