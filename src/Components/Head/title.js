346dc8e90882ba962a0633d369f1e47abe2a6516
Clarice Bouwer <cbillowes@gmail.com>
2021-05-19 05:55:30 +0400

    Separate page title with site title using a hyphen instead of a pipe

--- src/Components/Head/title.js
+++ src/Components/Head/title.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 
 const Title = ({ site, page }) => {
-  const title = (page && site && `${page} | ${site}`) || page || site
+  const title = (page && site && `${page} - ${site}`) || page || site
   return (
     <Helmet>
       <title>{title}</title>
