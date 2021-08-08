151ef7f6b72c1f26ab55d08b2eed68d9a3a8a599
Clarice Bouwer <cbillowes@gmail.com>
2021-08-08 05:13:52 +0400

    Move all files to playground
    Something is broken on Netlify https://app.netlify.com/sites/gallant-cray-096f77/deploys/610f2cbe14cf62b348b6b2bb. It's trying to do bundles and is failing. The site score is also shit on lighthouse so time for a do-over.

--- src/Components/Theme/themes/dark.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Colors from "../colors/dark"
-import { MdWbSunny } from "@react-icons/all-files/md/MdWbSunny"
-
-export default {
-  name: "dark",
-  icon: <MdWbSunny />,
-  color: { ...Colors },
-}
--- src/Components/Theme/themes/light.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Colors from "../colors/light"
-import { MdBrightness2 } from "@react-icons/all-files/md/MdBrightness2"
-
-export default {
-  name: "light",
-  icon: <MdBrightness2 />,
-  color: { ...Colors },
-}
