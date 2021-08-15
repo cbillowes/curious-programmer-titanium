7b41174bc46f9a02a90b83e1a80610d206d54fbd
Clarice Bouwer <cbillowes@gmail.com>
2021-08-15 21:21:04 +0400

    Save commented out credit in page

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -5,3 +5,6 @@ const CreditsPage = () => {
 }
 
 export default CreditsPage
+
+// Create a CSS Theme Switcher Using Gatsby and TailwindCSS
+// https://levelup.gitconnected.com/create-a-css-theme-switcher-using-gatsby-and-tailwindcss-aca85f93bd1f
