5576d7839c9e9d227075830fb4688465847edceb
Clarice Bouwer <cbillowes@gmail.com>
2021-08-15 20:04:52 +0400

    Mobile friendly personal introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,27 +15,27 @@ const Articles = ({ edges }) => {
       return (
         <div
           key={index}
-          className={`relative mt-32 flex mb-16 justify-center ${
-            index % 2 === 0 ? "flex-row-reverse" : "flex-row"
+          className={`relative md:mt-32 md:mb-16 p-5 flex justify-center flex-col-reverse ${
+            index % 2 === 0 ? "md:flex-row-reverse" : "md:flex-row"
           }`}
         >
           <div
-            className={`w-1/4 mx-16 ${
+            className={`md:w-1/4 md:mx-16 md:${
               index % 2 === 0 ? "text-left" : "text-right"
             }`}
           >
-            <h1 className="text-2xl leading-loose font-semibold">
+            <h1 className="text-xl mt-8 md:mt-0 md:text-2xl leading-loose font-semibold">
               {frontmatter.title}
             </h1>
             <div className="leading-loose mb-4">{node.excerpt}</div>
             <Anchor
-              className="bg-color-1 text-color-1-script rounded py-1 px-2 transform shadow-md"
+              className="bg-color-1 text-color-1-script rounded py-2 px-3 transform shadow-md"
               to={fields.slug}
             >
               Read more
             </Anchor>
           </div>
-          <div className="w-1/4">
+          <div className="md:w-1/4">
             <Thumbnail
               number={index}
               to={fields.slug}
@@ -61,19 +61,19 @@ const IndexPage = ({ data }) => {
     <LayoutWithoutConstraints meta={site.siteMetadata}>
       <div className="relative">
         <StaticImage src="../images/profile.jpg" alt="Clarice Bouwer" />
-        <div className="absolute top-10 right-10 flex flex-col flex-wrap justify-center items-center w-4/12">
-          <div className="bg-color-2 opacity-90 px-5 py-3 inline-block">
-            <h1 className="md:text-5xl xl:text-6xl text-color-2-script font-semibold text-center">
+        <div className="absolute top-2 right-2 md:top-10 md:right-10 flex flex-col flex-wrap justify-center items-center w-4/12">
+          <div className="bg-color-2 opacity-90 p-2 md:px-5 md:py-3">
+            <h1 className="text-sm md:text-5xl xl:text-6xl text-color-2-script font-semibold text-center">
               Clarice Bouwer
             </h1>
           </div>
-          <div className="hidden md:inline-block bg-color-1  opacity-90 px-5 py-3 mt-4">
-            <h2 className="md:text-3xl text-color-1-script font-semibold text-center">
+          <div className="bg-color-1 opacity-90 p-2 mt-2 md:px-5 md:py-3 md:mt-4">
+            <h2 className="text-xs md:text-3xl text-color-1-script font-semibold text-center">
               Senior Software Engineer
             </h2>
           </div>
-          <div className="bg-color-3 opacity-90 px-5 py-3 mt-4 inline-block">
-            <h2 className="md:text-2xl text-color-3-script font-semibold text-center">
+          <div className="bg-color-3 opacity-90 p-2 mt-2 md:px-5 md:py-3 md:mt-4">
+            <h2 className="text-xs md:text-2xl text-color-3-script font-semibold text-center">
               Cloudsure Ltd
             </h2>
           </div>
@@ -81,7 +81,7 @@ const IndexPage = ({ data }) => {
             <p>I write code. I share code. I love code.</p>
             <p>I am a curious programmer.</p>
           </div>
-          <div className="flex mt-8 items-center">
+          <div className="hidden md:flex mt-8 items-center">
             <Emoji
               className="text-5xl inline mr-4"
               label="South African flag emoji"
@@ -106,6 +106,10 @@ const IndexPage = ({ data }) => {
             </span>
           </div>
         </div>
+      </div>
+
+      <div>
+        Hello
       </div>
 
       <main className="bg-default text-default-script">
