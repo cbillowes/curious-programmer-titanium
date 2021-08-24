c9a368d1330804bde08a84040b07278a2c840a5f
Clarice Bouwer <cbillowes@gmail.com>
2021-08-24 11:46:28 +0400

    Increase dimensions of facebook and twitter images

--- src/components/Head/social.js
+++ src/components/Head/social.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 const getResource = (url, resource) => `${url}${resource || "/"}`
 
-const Social = ({ pageType, title, imagePath, url, route, twitter }) => {
+const Social = ({ pageType, imagePath, url, route, twitter }) => {
   const canonical = getResource(url, route)
   const image = `${url}${
     require(`../../images/social-media/${imagePath || "share.jpg"}`).default
@@ -14,17 +14,16 @@ const Social = ({ pageType, title, imagePath, url, route, twitter }) => {
       <link rel="canonical" href={canonical} />
       <meta name="image" content={image} />
 
-      <meta property="og:title" content={title} />
       <meta property="og:type" content={pageType || "website"} />
       <meta property="og:url" content={canonical} />
       <meta property="og:image" content={image} />
-      <meta property="og:image:width" content="800" />
-      <meta property="og:image:height" content="600" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
 
       <meta name="twitter:url" content={canonical} />
       <meta name="twitter:image" content={image} />
-      <meta name="twitter:image:width" content="800" />
-      <meta name="twitter:image:height" content="600" />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="630" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={`@${twitter}`} />
       <meta name="twitter:creator" content={`@${twitter}`} />
