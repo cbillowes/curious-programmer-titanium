5aeac142095ab7018647d2451d3e7658750b61cb
Clarice Bouwer <cbillowes@gmail.com>
2021-08-27 13:58:21 +0400

    Set search box to auto focus

--- src/components/Search/box.js
+++ src/components/Search/box.js
@@ -5,12 +5,13 @@ export default connectSearchBox(
   ({ refine, currentRefinement, className, onFocus, toggle }) => (
     <form className={`relative bg-color-2 ${className}`}>
       <input
-        className="py-4 bg-transparent placeholder-search-neutral-1 text-search-neutral-2 text-3xl lg:text-7xl border-b border-search-neutral-1 w-screen outline-none ml-4"
+        className="pl-4 py-4 bg-transparent placeholder-search-neutral-1 text-search-neutral-2 text-3xl lg:text-7xl border-b border-search-neutral-1 w-screen outline-none"
         type="text"
         placeholder="Search"
         aria-label="Search"
         onChange={(e) => refine(e.target.value)}
         value={currentRefinement}
+        autoFocus={true}
         onFocus={onFocus}
         onKeyUp={(e) => {
           if (e.key === "Escape") {
