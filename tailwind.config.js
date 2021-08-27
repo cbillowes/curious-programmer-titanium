f6a30bbdb5b93fae456e5ec8c690266d2550ab7a
Clarice Bouwer <cbillowes@gmail.com>
2021-08-27 07:31:47 +0400

    Fix footer colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,8 @@ module.exports = {
         "welcome-headings": "var(--welcome-headings)",
         "search-neutral-1": "var(--search-neutral-1)",
         "search-neutral-2": "var(--search-neutral-2)",
+        "footer-link-1": "var(--footer-link-1)",
+        "footer-link-2": "var(--footer-link-2)",
 
         neutral: "var(--neutral)",
 
