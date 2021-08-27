5e90b203e07f843308cf29c5b4f406853366bc36
Clarice Bouwer <cbillowes@gmail.com>
2021-08-27 13:41:49 +0400

    Add console easter egg

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -28,6 +28,35 @@ const SetColorModeScript = () => {
   return <script dangerouslySetInnerHTML={{ __html: javascript }} />
 }
 
+const SetConsoleEggScript = () => {
+  const javascript = `
+(function() {
+  // https://stackoverflow.com/questions/36885562/google-chrome-console-print-image
+  console.image = function(url, size = 100) {
+    var image = new Image();
+    image.onload = function() {
+      var style = [
+        'font-size: 1px;',
+        'padding: ' + this.height/100*size + 'px ' + this.width/100*size + 'px;',
+        'background: url('+ url +') no-repeat;',
+        'background-size: contain;',
+      ].join(' ');
+      console.log('%c ', style);
+      console.log('Be curious, always.');
+    };
+    image.src = url;
+  };
+  console.image("https://i.pinimg.com/originals/78/89/e9/7889e9fb00e971c7357d06725b7cde07.png", 25);
+})()
+`
+  return <script dangerouslySetInnerHTML={{ __html: javascript }} />
+}
+
 export const onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents(<SetColorModeScript />)
+  setPreBodyComponents(
+    <>
+      <SetColorModeScript />
+      <SetConsoleEggScript />
+    </>,
+  )
 }
