9c460ead8eeb1959241737cade7cb6adbeca2d70
Clarice Bouwer <cbillowes@gmail.com>
2021-08-27 11:29:35 +0400

    Fix: Increase the z-index of the search container to appear over the logo

--- src/components/Search/root.js
+++ src/components/Search/root.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 
 const Search = ({ children }) => {
   return (
-    <div className="fixed top-10 left-0 right-0 bottom-0 overflow-y-scroll overflow-x-hidden bg-default text-default-script z-10">
+    <div className="fixed top-10 left-0 right-0 bottom-0 overflow-y-scroll overflow-x-hidden bg-default text-default-script z-50">
       {children}
     </div>
   )
