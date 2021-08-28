f55d37ee08b59507c09534fb28dd1ca9733ef8d7
Clarice Bouwer <cbillowes@gmail.com>
2021-08-28 18:35:57 +0400

    Template Community Guidelines

--- src/pages/community.js
+++ src/pages/community.js
@@ -1,56 +1,98 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
+import { Layout } from "../components/Layout"
 import Anchor from "../components/Anchor"
+import "../styles/privacy.scss"
 
-const CommunityPage = () => {
+const CommunityPage = ({ data }) => {
+  const { site } = data
+  const { title } = site.siteMetadata
+
   return (
-    <div>
-      <h1>Community Guidelines</h1>
-      <p>Essentially, words can hurt. Be nice but clear!</p>
+    <Layout
+      meta={{
+        ...site.siteMetadata,
+        pageTitle: "Community Guidelines",
+        siteTitle: title,
+      }}
+    >
+      <div className="guidelines max-w-screen-md mx-auto p-4">
+        <h1>Community Guidelines</h1>
+        <p>Essentially, words can hurt. Be nice but clear!</p>
 
-      <h2>Things to know</h2>
-      <ul>
-        <li>
-          Our comments run on the third-party Disqus platform. You can read its
-          privacy policy, recently updated to reflect strict new privacy laws in
-          Europe that will ultimately be applied for all users within and
-          without Europe,{" "}
-          <Anchor to="https://help.disqus.com/terms-and-policies/disqus-privacy-policy">
-            here
-          </Anchor>
-          .
-        </li>
-        <li>
-          You can register a Disqus profile either directly with Disqus, or with
-          your Facebook, Twitter or Google accounts. Disqus does not require you
-          use your real name.
-        </li>
-        <li>
-          Your thoughts are important to me. I read, <strong>moderated</strong>{" "}
-          and publish all comments that are not filtered out as spam.
-        </li>
-      </ul>
+        <h2>Things to know</h2>
+        <ul>
+          <li>
+            Our comments run on the third-party Disqus platform. You can read
+            its privacy policy, recently updated to reflect strict new privacy
+            laws in Europe that will ultimately be applied for all users within
+            and without Europe,{" "}
+            <Anchor to="https://help.disqus.com/terms-and-policies/disqus-privacy-policy">
+              here
+            </Anchor>
+            .
+          </li>
+          <li>
+            You can register a Disqus profile either directly with Disqus, or
+            with your Facebook, Twitter or Google accounts. Disqus does not
+            require you use your real name.
+          </li>
+          <li>
+            Your thoughts are important to me. I read,{" "}
+            <strong>moderated</strong> and publish all comments that are not
+            filtered out as spam.
+          </li>
+        </ul>
 
-      <h2>Tone and Attitude</h2>
-      <ul>
-        <li>Be inviting, passionate and share your thoughts.</li>
-        <li>Please do not personally attack or use hurtful words.</li>
-        <li>
-          Do not criticize a brand, company or product, and attacking people on
-          the basis of their race, religion, sex, gender, sexual orientation,
-          disability or age.
-        </li>
-        <li>No racism, sexism, homophobia or other forms of hate-speech!</li>
-      </ul>
+        <h2>Tone and Attitude</h2>
+        <ul>
+          <li>Be inviting, passionate and share your thoughts.</li>
+          <li>Please do not personally attack or use hurtful words.</li>
+          <li>
+            Do not criticize a brand, company or product, and attacking people
+            on the basis of their race, religion, sex, gender, sexual
+            orientation, disability or age.
+          </li>
+          <li>No racism, sexism, homophobia or other forms of hate-speech!</li>
+        </ul>
 
-      <h2>Clarity and Intent</h2>
-      <ul>
-        <li>Be clear and intentional to avoid ambiguity in your messages.</li>
-        <li>
-          Do not &ldquo;shout&rdquo;, be sarcastic, condescending or mean.
-        </li>
-      </ul>
-    </div>
+        <h2>Clarity and Intent</h2>
+        <ul>
+          <li>Be clear and intentional to avoid ambiguity in your messages.</li>
+          <li>
+            Do not &ldquo;shout&rdquo;, be sarcastic, condescending or mean.
+          </li>
+        </ul>
+      </div>
+    </Layout>
   )
+}
+
+export const query = graphql`
+  query CommunityPageQuery {
+    site {
+      siteMetadata {
+        author {
+          name
+          url
+        }
+        brand
+        description
+        keywords
+        lang
+        title
+        url
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
+
+CommunityPage.propTypes = {
+  data: PropTypes.object.isRequired,
 }
 
 export default CommunityPage
