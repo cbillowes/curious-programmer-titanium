7d771ff0b97a548b817dc20551577c6f1d46cb5b
Clarice Bouwer <cbillowes@gmail.com>
2021-08-28 04:47:06 +0400

    Add cross origin attribute to preconnect of fonts.gstatic

--- src/components/Head/font.js
+++ src/components/Head/font.js
@@ -3,7 +3,11 @@ import { Helmet } from "react-helmet"
 
 const Font = () => (
   <Helmet>
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossOrigin={true}
+    />
     <link
       href="//fonts.googleapis.com/css2?family=Open+Sans:wght@300;700&family=Courier+Prime&family=Handlee&display=swap"
       rel="stylesheet"
