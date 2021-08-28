545c031f040aca46a49da073e9b339b2f759def2
Clarice Bouwer <cbillowes@gmail.com>
2021-08-28 18:29:27 +0400

    Add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,117 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
+import { Layout } from "../components/Layout"
+import List from "../components/Articles/List"
+import Anchor from "../components/Anchor"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+const Articles = ({ edges }) => {
+  return <List edges={edges} />
 }
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+const NotFoundPage = ({ data }) => {
+  const { allMarkdownRemark, site } = data
+  const edges = allMarkdownRemark.edges
+  const { title } = site.siteMetadata
 
-// markup
-const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout
+      meta={{
+        ...site.siteMetadata,
+        pageTitle: "Welcome",
+        siteTitle: title,
+      }}
+    >
+      <div className="pt-16">
+        <div className="text-center">
+          <h1 className="text-md lg:text-5xl xl:text-8xl text-center mb-4">
+            <span className="font-semibold">Sorry!</span> Can’t find what you
+            are looking for
+          </h1>
+          <p className="text-2xl">
+            You can check out the latest articles instead.
+          </p>
+        </div>
+        <div className="bg-default text-default-script">
+          <div className="mx-auto pb-16 text-center">
+            <Articles edges={edges} />
+            <Anchor
+              to="/blog"
+              title="All articles"
+              className="bg-color-1-alternative text-color-1-script rounded py-1 px-3 transform shadow-md hover:bg-color-1"
+            >
+              Discover more
+            </Anchor>
+          </div>
+        </div>
+      </div>
+    </Layout>
   )
 }
+
+NotFoundPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array.isRequired,
+    }).isRequired,
+
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.object.isRequired,
+    }).isRequired,
+  }).isRequired,
+}
+
+Articles.propTypes = {
+  edges: PropTypes.array.isRequired,
+  wordCount: PropTypes.number,
+}
+
+export const query = graphql`
+  query NotFoundPageQuery {
+    allMarkdownRemark(
+      limit: 10
+      filter: { fields: { slug: { nin: "/blog/example/" } } }
+      sort: { order: DESC, fields: fields___number }
+    ) {
+      edges {
+        node {
+          timeToRead
+          excerpt(truncate: true, pruneLength: 200, format: PLAIN)
+          fields {
+            slug
+            date(formatString: "LL")
+            number
+            component
+            photo
+            credit
+            source
+            link
+          }
+          frontmatter {
+            title
+            tags
+          }
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author {
+          name
+          url
+        }
+        brand
+        description
+        keywords
+        lang
+        title
+        url
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
 
 export default NotFoundPage
