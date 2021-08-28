e0b7b58a49eefa0adc55fcf728e2cee7f700cb18
Clarice Bouwer <cbillowes@gmail.com>
2021-08-28 04:23:48 +0400

    Remove manifest.json url from head

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -36,7 +36,6 @@ const Head = ({
         lang,
       }}
     >
-      <link rel="manifest" href={`${url}/manifest.json`} />
       <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="Content-Type" content="text/html; charset=UTF-8" />
