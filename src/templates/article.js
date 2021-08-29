756c1535893124bad1f245d964f6f4af3b06bcfc
Clarice Bouwer <cbillowes@gmail.com>
2021-08-29 22:09:13 +0400

    Add a thumbnail to the top of the article

--- src/templates/article.js
+++ src/templates/article.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import PropTypes from "prop-types"
 import { Layout } from "../components/Layout"
+import Thumbnail from "../components/Thumbnail"
 import Tags from "../components/Tags"
 import "../styles/article.scss"
 // gatsby-remark-embed-gist
@@ -60,6 +61,7 @@ const ArticleTemplate = ({ data }) => {
   const { markdownRemark, site } = data
   const { excerpt, timeToRead, html, fields, frontmatter } = markdownRemark
   const { title, description, url } = site.siteMetadata
+  const { component, photo, credit, link, source } = fields
 
   return (
     <Layout
@@ -73,6 +75,16 @@ const ArticleTemplate = ({ data }) => {
       }}
     >
       <div id="article" className="pt-14 px-4 pb-24">
+        <div className="w-3/12 mx-auto overflow-hidden">
+          <Thumbnail
+            alt={title}
+            photo={photo}
+            credit={credit}
+            source={source}
+            link={link}
+            componentName={component}
+          />
+        </div>
         <h1 className="text-center font-bold px-4 md:px-10 max-w-screen-xl mx-auto">
           {frontmatter.title}
         </h1>
